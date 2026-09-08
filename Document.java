051fad0dcbab2569aeb342b0bfb1229b84d6a86e
SOULEYMANE_KOUSSE <koussesouleymane861@gmail.com>
2026-09-08 22:42:15 +0000

    first commit

---
+++ Document.java
@@ -0,0 +1,36 @@
+public class Document {
+
+    private int id;
+    private String titre;
+    private String auteur;
+    private boolean disponible;
+
+    public Document(int id, String titre, String auteur) {
+        this.id = id;
+        this.titre = titre;
+        this.auteur = auteur;
+        this.disponible = true;
+    }
+
+
+    public boolean estDisponible() {
+        return disponible;
+    }
+
+    public void emprunter() {
+        disponible = false;
+    }
+
+    public void retourner() {
+        disponible = true;
+    }
+
+    public void afficher() {
+        System.out.println("Document n°" + id +" titre "+ titre +" auteur " + auteur);
+        if (disponible) {
+            System.out.println("Disponible");
+        } else {
+            System.out.println("Indisponible");
+        }
+    }
+}
