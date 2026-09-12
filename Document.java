a4b95e39b48c93bc9b2c55a82da91155b78de395
SOULEYMANE_KOUSSE <koussesouleymane861@gmail.com>
2026-09-12 14:03:38 +0000

    Update Document.java
    document corrigé

--- Document.java
+++ Document.java
@@ -1,27 +1,44 @@
-public class Document {
+public abstract class Document {
 
-    private int id;
-    private String titre;
-    private String auteur;
+    private int     numero;
+    private String  titre;
+    private String  auteurPrincipal;
     private boolean disponible;
+    private Lecteur emprunteur;
 
-    public Document(int id, String titre, String auteur) {
-        this.id = id;
-        this.titre = titre;
-        this.auteur = auteur;
-        this.disponible = true;
+    public Document(int numero, String titre, String auteurPrincipal) {
+        this.numero          = numero;
+        this.titre           = titre;
+        this.auteurPrincipal = auteurPrincipal;
+        this.disponible      = true;
+    }
+
+    public int     getNumero()          { return numero; }
+    public String  getTitre()           { return titre; }
+    public String  getAuteurPrincipal() { return auteurPrincipal; }
+
+
+    public boolean getDisponible() {
+        return disponible;
     }
 
 
-    public void emprunter() {
-        disponible = false;
+    public abstract int dureeMaxPret();
+
+    public void emprunter(Lecteur l) {
+        this.disponible = false;
+        this.emprunteur = l;
+    }
+    public Lecteur getEmprunteur() {
+        return emprunteur;
     }
 
     public void retourner() {
-        disponible = true;
+        this.disponible = true;
+        this.emprunteur = null;
     }
 
-   @Override
+    @Override
     public String toString() {
         return "Document n° " + numero + "Titre : " + titre + " Auteur : " + auteurPrincipal+ ".";
     }
